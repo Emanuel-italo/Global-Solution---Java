3c6d017a84a48d48a1f556ca85f982f31966af17
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-27 21:33:00 -0300

    feat: cria enum TipoAlertaEnum

---
+++ src/main/java/br/com/orbitlink/space/enums/TipoAlertaEnum.java
@@ -0,0 +1,9 @@
+package br.com.orbitlink.space.enums;
+
+public enum TipoAlertaEnum {
+    RISCO_COLISAO,
+    DEGRADACAO_SISTEMA,
+    FALHA_COMUNICACAO,
+    ANOMALIA_ORBITAL,
+    ALERTA_CLIMATICO
+}
