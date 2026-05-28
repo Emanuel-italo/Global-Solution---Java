4247104c2a090b8e284aaa05d9ff3d12e335df7b
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-27 21:35:11 -0300

    feat: cria exceção personalizada RegraDeNegocioException

---
+++ src/main/java/br/com/orbitlink/space/exception/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package br.com.orbitlink.space.exception;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
