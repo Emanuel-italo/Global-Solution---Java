42900cd2da09b6e0361b99840bc3a89a5e012634
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-27 21:33:57 -0300

    feat: cria exceção personalizada EntidadeNaoLocalizadaException

---
+++ src/main/java/br/com/orbitlink/space/exception/EntidadeNaoLocalizadaException.java
@@ -0,0 +1,7 @@
+package br.com.orbitlink.space.exception;
+
+public class EntidadeNaoLocalizadaException extends RuntimeException {
+    public EntidadeNaoLocalizadaException(String message) {
+        super(message);
+    }
+}
