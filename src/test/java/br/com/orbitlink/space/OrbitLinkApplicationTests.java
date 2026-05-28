6bf91b75c2698f0afb96274971eba2ceb7a038ff
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-27 21:52:25 -0300

    feat: cria classe de teste de carga de contexto Spring Boot para o projeto OrbitLink

---
+++ src/test/java/br/com/orbitlink/space/OrbitLinkApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.orbitlink.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
