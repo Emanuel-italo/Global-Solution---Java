2e369afaac56b5f75528b366311ec08aff219243
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 11:15:10 -0300

    atualizando o usuario repository

--- src/main/java/br/com/orbitlink/space/security/UsuarioRepository.java
+++ src/main/java/br/com/orbitlink/space/security/UsuarioRepository.java
@@ -1,4 +1,8 @@
 package br.com.orbitlink.space.security;
 
-public class UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
