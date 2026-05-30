51955f0b28af84e9967c8b028e7b95038a62dfe9
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 11:30:12 -0300

    feat(repository): implementar descomissionamento de Ativo Espacial via JPA

--- src/main/java/br/com/orbitlink/space/repository/AtivoEspacialRepository.java
+++ src/main/java/br/com/orbitlink/space/repository/AtivoEspacialRepository.java
@@ -1,4 +1,16 @@
 package br.com.orbitlink.space.repository;
 
-public class AtivoEspacialRepository {
+import br.com.orbitlink.space.entity.AtivoEspacial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AtivoEspacialRepository extends JpaRepository<AtivoEspacial, Long> {
+
+    // Substitui o método "descomissionar" que você tinha no DAO antigo
+    @Modifying
+    @Query("UPDATE AtivoEspacial a SET a.operacional = false WHERE a.id = :id")
+    int descomissionarAtivo(Long id);
 }
