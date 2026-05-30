efae3d90a2a4a8a07c993404d2c4a94c47deb7e5
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 11:14:04 -0300

    entregando o equipamento orbital

--- src/main/java/br/com/orbitlink/space/entity/EquipamentoOrbital.java
+++ src/main/java/br/com/orbitlink/space/entity/EquipamentoOrbital.java
@@ -1,4 +1,21 @@
 package br.com.orbitlink.space.entity;
 
-public class EquipamentoOrbital {
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "TB_EQUIPAMENTO_ORBITAL")
+@Inheritance(strategy = InheritanceType.JOINED) // Estratégia de herança exigida
+@Data
+public abstract class EquipamentoOrbital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nomeProprietario;
+
+    @Column(nullable = false)
+    private String numeroDeSerie;
 }
