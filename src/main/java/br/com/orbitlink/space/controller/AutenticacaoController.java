c878176746e387016eb59c577d29d59768d40255
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 15:24:43 -0300

    adicionndo autentificacao no controller

--- src/main/java/br/com/orbitlink/space/controller/AutenticacaoController.java
+++ src/main/java/br/com/orbitlink/space/controller/AutenticacaoController.java
@@ -1,4 +1,26 @@
 package br.com.orbitlink.space.controller;
 
+import br.com.orbitlink.space.dto.AutenticacaoRequest;
+import br.com.orbitlink.space.dto.TokenResponse;
+import br.com.orbitlink.space.security.TokenService;
+import br.com.orbitlink.space.security.Usuario;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Autenticação", description = "Endpoints para login e geração de token")
 public class AutenticacaoController {
+
+    
+
 }
