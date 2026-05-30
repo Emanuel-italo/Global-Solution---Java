fc7179733f7a6daebde509359d7502898a6edc1e
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 15:19:42 -0300

    trazendo o spring security, para o login e senha

--- src/main/java/br/com/orbitlink/space/ProjectApplication.java
+++ src/main/java/br/com/orbitlink/space/ProjectApplication.java
@@ -1,9 +1,24 @@
 package br.com.orbitlink.space;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(title = "OrbitLink API", version = "1.0", description = "API de Gerenciamento da Economia Espacial"),
+        security = @SecurityRequirement(name = "bearerAuth") 
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class ProjectApplication {
 
     public static void main(String[] args) {
