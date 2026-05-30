9e309d1c1a6e0f541fad0fc694bf856fafd939ea
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 12:16:06 -0300

    instanciando os contrutores

--- src/main/java/br/com/orbitlink/space/entity/RegistroTelemetria.java
+++ src/main/java/br/com/orbitlink/space/entity/RegistroTelemetria.java
@@ -57,5 +57,17 @@ public class RegistroTelemetria {
         this.observacaoGps = observacaoGps;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AtivoEspacial getAtivoEspacial() {
+        return ativoEspacial;
+    }
+
 
 }
