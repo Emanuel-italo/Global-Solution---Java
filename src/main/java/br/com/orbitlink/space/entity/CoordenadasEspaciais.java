fa6b8a4acaff195aef2845fd3f4c1a1abbf4741d
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 11:13:41 -0300

    atualizando coordenadas

--- src/main/java/br/com/orbitlink/space/entity/CoordenadasEspaciais.java
+++ src/main/java/br/com/orbitlink/space/entity/CoordenadasEspaciais.java
@@ -1,4 +1,12 @@
 package br.com.orbitlink.space.entity;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable // Componente reutilizável dentro de uma Entidade
+@Data
 public class CoordenadasEspaciais {
+    private Double latitude;
+    private Double longitude;
+    private Double altitudeEmKm;
 }
