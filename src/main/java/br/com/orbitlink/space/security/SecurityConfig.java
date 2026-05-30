58ffb01ce73bc17647b5f18323b79975926ac1cb
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 16:20:43 -0300

    editando o security

--- src/main/java/br/com/orbitlink/space/security/SecurityConfig.java
+++ src/main/java/br/com/orbitlink/space/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
