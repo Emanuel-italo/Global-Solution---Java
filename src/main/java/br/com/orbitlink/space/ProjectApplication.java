d26129811f6866547c32f0805576d8b1e4d5c3b6
Emanuel Italo <emanuelitaloleal@hotmail.com>
2026-05-30 11:58:29 -0300

    adicionar classe principal do Spring Boot

--- src/main/java/br/com/orbitlink/space/ProjectApplication.java
+++ src/main/java/br/com/orbitlink/space/ProjectApplication.java
@@ -1,34 +1,12 @@
 package br.com.orbitlink.space;
 
-import br.com.orbitlink.space.entity.AlertaOrbital;
-import br.com.orbitlink.space.entity.AtivoEspacial;
-import br.com.orbitlink.space.entity.ManutencaoOrbital;
-import br.com.orbitlink.space.entity.RegistroTelemetria;
-import br.com.orbitlink.space.util.MotorReflectionOrbitLink;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication implements CommandLineRunner {
-
-    private final MotorReflectionOrbitLink motorReflectionOrbitLink;
-
-    public ProjectApplication(MotorReflectionOrbitLink motorReflectionOrbitLink) {
-        this.motorReflectionOrbitLink = motorReflectionOrbitLink;
-    }
+public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        motorReflectionOrbitLink.inspecionar(
-                AtivoEspacial.class,
-                ManutencaoOrbital.class,
-                RegistroTelemetria.class,
-                AlertaOrbital.class
-        );
     }
 }
